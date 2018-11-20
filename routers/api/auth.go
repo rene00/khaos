8294b263ed994aab8d3cfb3c6ec57ddcfd44d6f7
Rene Cunningham <rene@compounddata.com>
2018-11-20 11:08:07 +0000

    commit routers dir.

---
+++ routers/api/auth.go
@@ -0,0 +1,56 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/astaxie/beego/validation"
+	"github.com/gin-gonic/gin"
+
+	"github.com/rene00/khaos/pkg/app"
+	"github.com/rene00/khaos/pkg/e"
+	"github.com/rene00/khaos/pkg/util"
+	"github.com/rene00/khaos/service/auth_service"
+)
+
+type auth struct {
+	Username string `valid:"Required; MaxSize(50)"`
+	Password string `valid:"Required; MaxSize(50)"`
+}
+
+func GetAuth(c *gin.Context) {
+	appG := app.Gin{C: c}
+	valid := validation.Validation{}
+
+	username := c.Query("username")
+	password := c.Query("password")
+
+	a := auth{Username: username, Password: password}
+	ok, _ := valid.Valid(&a)
+
+	if !ok {
+		appG.Response(http.StatusOK, e.INVALID_PARAMS, nil)
+		return
+	}
+
+	authService := auth_service.Auth{Username: username, Password: password}
+	isExist, err := authService.Check()
+	if err != nil {
+		appG.Response(http.StatusOK, e.ERROR_AUTH_CHECK_TOKEN_FAIL, nil)
+		return
+	}
+
+	if !isExist {
+		appG.Response(http.StatusOK, e.ERROR_AUTH, nil)
+		return
+	}
+
+	token, err := util.GenerateToken(username, password)
+	if err != nil {
+		appG.Response(http.StatusOK, e.ERROR_AUTH_TOKEN, nil)
+		return
+	}
+
+	appG.Response(http.StatusOK, e.SUCCESS, map[string]string{
+		"token": token,
+	})
+}
