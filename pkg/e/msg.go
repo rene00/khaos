7a560a836ef686d3e2a90627459d1b1fb93c03bb
Rene Cunningham <rene@compounddata.com>
2018-11-22 04:00:43 +0000

    Added 2 more errors

--- pkg/e/msg.go
+++ pkg/e/msg.go
@@ -3,11 +3,13 @@ package e
 var MsgFlags = map[int]string{
 	SUCCESS:                        "ok",
 	ERROR:                          "fail",
+	UNAUTHORIZED:                   "unauthorized",
 	INVALID_PARAMS:                 "invalid params",
 	ERROR_AUTH_CHECK_TOKEN_FAIL:    "token fail",
 	ERROR_AUTH_CHECK_TOKEN_TIMEOUT: "token timeout",
 	ERROR_AUTH_TOKEN:               "token error",
 	ERROR_AUTH:                     "auth error",
+	ERROR_PING:                     "ping fail",
 }
 
 func GetMsg(code int) string {
