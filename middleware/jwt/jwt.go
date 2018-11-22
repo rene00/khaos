11ecb8c987364db00c3482ee1b1803e7767a610c
Rene Cunningham <rene@compounddata.com>
2018-11-22 03:52:30 +0000

    More jwt token discovery to use json data and not url params.

--- middleware/jwt/jwt.go
+++ middleware/jwt/jwt.go
@@ -2,6 +2,7 @@ package jwt
 
 import (
 	"net/http"
+	"regexp"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
@@ -16,18 +17,46 @@ func JWT() gin.HandlerFunc {
 		var data interface{}
 
 		code = e.SUCCESS
-		token := c.Query("token")
-		if token == "" {
-			code = e.INVALID_PARAMS
-		} else {
-			_, err := util.ParseToken(token)
-			if err != nil {
-				switch err.(*jwt.ValidationError).Errors {
-				case jwt.ValidationErrorExpired:
-					code = e.ERROR_AUTH_CHECK_TOKEN_TIMEOUT
-				default:
-					code = e.ERROR_AUTH_CHECK_TOKEN_FAIL
-				}
+
+		authHeader := c.Request.Header.Get("Authorization")
+		r, _ := regexp.Compile("^Bearer (.+)$")
+
+		match := r.FindStringSubmatch(authHeader)
+
+		if len(match) == 0 {
+			code = e.UNAUTHORIZED
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"code": code,
+				"msg":  e.GetMsg(code),
+				"data": data,
+			})
+
+			c.Abort()
+			return
+		}
+
+		token := match[1]
+
+		if len(token) == 0 {
+			code = e.ERROR_AUTH_TOKEN
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"code": code,
+				"msg":  e.GetMsg(code),
+				"data": data,
+			})
+
+			c.Abort()
+			return
+		}
+
+		_, err := util.ParseToken(token)
+
+		if err != nil {
+			switch err.(*jwt.ValidationError).Errors {
+			case jwt.ValidationErrorExpired:
+				code = e.ERROR_AUTH_CHECK_TOKEN_TIMEOUT
+			default:
+				code = e.ERROR_AUTH_CHECK_TOKEN_FAIL
 			}
 		}
 
