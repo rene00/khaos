b1cc690c7e8fe7927f3fe7b329e415f85b0ef8f1
Rene Cunningham <rene@compounddata.com>
2018-11-22 04:02:32 +0000

    Add ping to database.

--- routers/api/v1/ping.go
+++ routers/api/v1/ping.go
@@ -6,11 +6,31 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/rene00/khaos/pkg/app"
 	"github.com/rene00/khaos/pkg/e"
+	"github.com/rene00/khaos/service/ping_service"
 )
 
+type PingBody struct {
+	Username string `json:"username"`
+}
+
 func Ping(c *gin.Context) {
+	var pingBody PingBody
 	appG := app.Gin{C: c}
+
 	data := make(map[string]interface{})
-	data["message"] = "pong"
+	data["response"] = "pong"
+
+	c.BindJSON(&pingBody)
+
+	pingService := ping_service.Ping{
+		Username: pingBody.Username,
+	}
+
+	err := pingService.Add()
+	if err != nil {
+		appG.Response(http.StatusOK, e.ERROR_PING, nil)
+		return
+	}
+
 	appG.Response(http.StatusOK, e.SUCCESS, data)
 }
