99562465e973ee73cb62d4d673240e4190ad824f
Rene Cunningham <rene@compounddata.com>
2018-12-06 02:59:51 +0000

    Expose way to save campaigns via model.

--- models/campaign.go
+++ models/campaign.go
@@ -42,43 +42,72 @@ type CampaignResult struct {
 	Name string `gorm:"unique;not null"`
 }
 
+// Save the campaign to the datastore.
+func (c *Campaign) Save() (*Campaign, error) {
+	if err := db.Save(c).Error; err != nil {
+		return c, err
+	}
+	return c, nil
+}
+
+func GetCampaign(campaignID uint) (Campaign, error) {
+	var campaign Campaign
+	if err := db.Where("id = ?", campaignID).First(&campaign).Error; err != nil {
+		return campaign, errors.New("Campaign does not exist")
+	}
+
+	// Get Attack
+	attack, err := GetAttack(campaign.AttackID)
+	if err != nil {
+		return campaign, err
+	}
+	campaign.Attack = attack
+
+	// Get Resource
+	resource, err := GetResource(campaign.ResourceID)
+	if err != nil {
+		return campaign, err
+	}
+	campaign.Resource = resource
+
+	// Get Status
+	campaignStatus, err := GetCampaignStatus(campaign.CampaignStatusID)
+	if err != nil {
+		return campaign, err
+	}
+	campaign.CampaignStatus = campaignStatus
+
+	// Get Result
+	campaignResult, err := GetCampaignResult(campaign.CampaignResultID)
+	if err != nil {
+		return campaign, err
+	}
+	campaign.CampaignResult = campaignResult
+
+	return campaign, nil
+}
+
 func GetCampaigns(authID uint) ([]Campaign, error) {
 	var campaigns []Campaign
 	db.Where(&Campaign{AuthID: authID}).Find(&campaigns)
 	c := campaigns[:0]
 	for _, campaign := range campaigns {
-
-		// Get Attack
-		attack, err := GetAttack(campaign.AttackID)
+		campaign, err := GetCampaign(campaign.ID)
 		if err != nil {
 			return campaigns, err
 		}
-		campaign.Attack = attack
-
-		// Get Resource
-		resource, err := GetResource(campaign.ResourceID)
-		if err != nil {
-			return campaigns, err
-		}
-		campaign.Resource = resource
-
-		// Get Status
-		campaignStatus, err := GetCampaignStatus(campaign.CampaignStatusID)
-		if err != nil {
-			return campaigns, err
-		}
-		campaign.CampaignStatus = campaignStatus
-
-		// Get Result
-		campaignResult, err := GetCampaignResult(campaign.CampaignResultID)
-		if err != nil {
-			return campaigns, err
-		}
-		campaign.CampaignResult = campaignResult
-
 		c = append(c, campaign)
 	}
 	return c, nil
+}
+
+func GetCampaignStatusByName(campaignStatusName string) (CampaignStatus, error) {
+	var campaignStatus CampaignStatus
+	err := db.Where("name = ?", campaignStatusName).First(&campaignStatus).Error
+	if err != nil {
+		return campaignStatus, errors.New("Failed getting campaign status")
+	}
+	return campaignStatus, nil
 }
 
 func GetCampaignStatus(campaignStatusID uint) (CampaignStatus, error) {
@@ -97,4 +126,12 @@ func GetCampaignResult(campaignResultID uint) (CampaignResult, error) {
 		return campaignResult, errors.New("Failed getting campaign result")
 	}
 	return campaignResult, nil
+}
+
+func (c *Campaign) EnableDisable(authID uint, enabled bool) error {
+	c.Enabled = enabled
+	if err := db.Update(c).Error; err != nil {
+		return err
+	}
+	return nil
 }
